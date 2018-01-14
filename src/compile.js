615ea2e96e409698ec0dbac8df72d85fa82b7fec
wmu <wmuisnan@gmail.com>
2018-01-14 17:00:54 +0800

    init

---
+++ src/compile.js
@@ -0,0 +1,80 @@
+'use strict';
+var _ = require('lodash');
+var $  = require('jquery');
+
+
+
+function $CompileProvider($provide) {
+  var hasDirectives = {};
+
+
+
+
+  this.directive = function (name, directiveFactory) {
+    if (_.isString(name)) {
+      if (name === 'hasOwnProperty') {
+        throw 'hasOwnProperty is not a valid directive name';
+      }
+      if (!hasDirectives.hasOwnProperty(name)) {
+        hasDirectives[name] = [];
+        $provide.factory(name + 'Directive', ['$injector', function ($injector) {
+          var factories = hasDirectives[name];
+          return _.map(factories, $injector.invoke);
+        }]);
+
+      }
+      hasDirectives[name].push(directiveFactory);
+    } else {
+      _.forEach(name, _.bind(function (directiveFactory, name) {
+        this.directive(name, directiveFactory);
+      }, this));
+    }
+
+  };
+
+  this.$get = ['$injector', function ($injector) {
+
+    function addDirective(directives, name) {
+      if (hasDirectives.hasOwnProperty(name)) {
+        directives.push.apply(directives, $injector.get(name + 'Directive'));
+      }
+    }
+
+    function nodeName(element) {
+      return element.nodeName ? element.nodeName : element[0].nodeName;
+    }
+
+    function compile($compileNodes) {
+      return compileNodes($compileNodes);
+    }
+
+    function compileNodes($compileNodes) {
+      _.forEach($compileNodes, function (node) {
+        var directives = collectDirectives(node);
+        applyDirectivesToNode(directives, node);
+      });
+    }
+
+    function collectDirectives(node) {
+      var directives = [];
+      var normalizedNodeName = _.camelCase(nodeName(node).toLowerCase());
+      addDirective(directives, normalizedNodeName);
+      return directives;
+    }
+
+    function applyDirectivesToNode(directives, compileNode) {
+      var $compileNode = $(compileNode);
+      _.forEach(directives, function (directive) {
+        if (directive.compile) {
+          directive.compile($compileNode);
+        }
+      });
+    }
+
+    return compile;
+  }];
+}
+
+$CompileProvider.$inject = ['$provide'];
+
+module.exports = $CompileProvider;
---
+++ test/compile_spec.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var _ = require('lodash');
+var $  = require('jquery');
+// var sinon = require('sinon');
+var publishExternalAPI = require('../src/angular_public');
+var createInjector = require('../src/injector');
+
+function makeInjectorWithDirectives() {
+  var args = arguments;
+  return createInjector(['ng', function ($compileProvider) {
+    $compileProvider.directive.apply($compileProvider, args);
+  }]);
+}
+
+
+describe('$compile', function () {
+
+  beforeEach(function () {
+    delete window.angular;
+    publishExternalAPI();
+  });
+
+
+
+  it('allows creating directives', function () {
+    var myModule = window.angular.module('myModule', []);
+    myModule.directive('testing', function () { });
+    var injector = createInjector(['ng', 'myModule']);
+    expect(injector.has('testingDirective')).toBe(true);
+  });
+
+  it('allows creating many directives with the same name', function () {
+    var myModule = window.angular.module('myModule', []);
+    myModule.directive('testing', _.constant({ d: 'one' }));
+    myModule.directive('testing', _.constant({ d: 'two' }));
+    var injector = createInjector(['ng', 'myModule']);
+    var result = injector.get('testingDirective');
+    expect(result.length).toBe(2);
+    expect(result[0].d).toEqual('one');
+    expect(result[1].d).toEqual('two');
+  });
+
+  it('does not allow a directive called hasOwnProperty', function () {
+    var myModule = window.angular.module('myModule', []);
+    myModule.directive('hasOwnProperty', function () { });
+    expect(function () {
+      createInjector(['ng', 'myModule']);
+    }).toThrow();
+  });
+
+
+  it('allows creating directives with object notation', function () {
+    var myModule = window.angular.module('myModule', []);
+    myModule.directive({
+      a: function () { },
+      b: function () { },
+      c: function () { }
+    });
+    var injector = createInjector(['ng', 'myModule']);
+    expect(injector.has('aDirective')).toBe(true);
+    expect(injector.has('bDirective')).toBe(true);
+    expect(injector.has('cDirective')).toBe(true);
+  });
+
+  it('compiles element directives from a single element', function () {
+    var injector = makeInjectorWithDirectives('myDirective', function () {
+      return {
+        compile: function (element) {
+          element.data('hasCompiled', true);
+        }
+      };
+    });
+    injector.invoke(function ($compile) {
+      var el = $('<my-directive></my-directive>');
+      $compile(el);
+      expect(el.data('hasCompiled')).toBe(true);
+    });
+  });
+
+  it('compiles element directives found from several elements', function () {
+    var idx = 1;
+    var injector = makeInjectorWithDirectives('myDirective', function () {
+      return {
+        compile: function (element) {
+          element.data('hasCompiled', idx++);
+        }
+      };
+    });
+    injector.invoke(function ($compile) {
+      var el = $('<my-directive></my-directive><my-directive></my-directive>');
+      $compile(el);
+      expect(el.eq(0).data('hasCompiled')).toBe(1);
+      expect(el.eq(1).data('hasCompiled')).toBe(2);
+    });
+  });
+
+
+});
