759342ad0e30d4319406f7697c3500b6e934d0ca
wmu <wmuisnan@gmail.com>
2018-01-14 23:18:21 +0800

    Overriding attributes with ng-attr

--- src/compile.js
+++ src/compile.js
@@ -147,7 +147,8 @@ function $CompileProvider($provide) {
           var attrStartName, attrEndName;
           var name = attr.name;
           var normalizedAttrName = directiveNormalize(name.toLowerCase());
-          if (/^ngAttr[A-Z]/.test(normalizedAttrName)) {
+          var isNgAttr = /^ngAttr[A-Z]/.test(normalizedAttrName);
+          if (isNgAttr) {
             name = _.kebabCase(
               normalizedAttrName[6].toLowerCase() +
               normalizedAttrName.substring(7)
@@ -163,10 +164,15 @@ function $CompileProvider($provide) {
           }
           normalizedAttrName = directiveNormalize(name.toLowerCase());
           addDirective(directives, normalizedAttrName, 'A', attrStartName, attrEndName);
-          attrs[normalizedAttrName] = attr.value.trim();
-          if (isBooleanAttribute(node, normalizedAttrName)) {
-            attrs[normalizedAttrName] = true;
+
+          // 在这个判断条件下，后面的属性不会覆盖前者。 whatever="43"  whatever="41"(或者DOM.attrs会合并同名？)
+          if (isNgAttr || !attrs.hasOwnProperty(normalizedAttrName)) { 
+            attrs[normalizedAttrName] = attr.value.trim();
+            if (isBooleanAttribute(node, normalizedAttrName)) {
+              attrs[normalizedAttrName] = true;
+            }            
           }
+
         });
         _.forEach(node.classList, function (cls) {
           var normalizedClassName = directiveNormalize(cls);
