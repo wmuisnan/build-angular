82c0d9d66401f9771e0cfcd51bd062dc021039a9
wmu <wmuisnan@gmail.com>
2018-01-14 19:09:17 +0800

    Applying Directives to Classes

--- src/compile.js
+++ src/compile.js
@@ -86,6 +86,10 @@ function $CompileProvider($provide) {
         }
         addDirective(directives, normalizedAttrName);
       });
+      _.forEach(node.classList, function(cls) {
+        var normalizedClassName = directiveNormalize(cls);
+        addDirective(directives, normalizedClassName);
+      });
       return directives;
     }
 
